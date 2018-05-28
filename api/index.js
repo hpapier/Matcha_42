f1a1c3590141aafd6b82a293fdac8917cddc4cf0
hpapier <hpapier@student.42.fr>
2018-05-28 15:49:52 +0200

    PostgreSQL connection test

--- api/index.js
+++ api/index.js
@@ -1,1 +1,11 @@
-console.log('Running');
+const http = require('http');
+const pg = require('pg');
+const host = 'postgres://postgres@127.0.0.1:5432/postgres';
+
+const client = new pg.Client(host);
+client.connect();
+
+http.createServer((req, res) => {
+    res.write('Hello');
+    res.end();
+}).listen(8080);
