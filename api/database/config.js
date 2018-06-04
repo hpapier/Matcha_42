f8574a1754b3c0aa77b600df60178cdd85d98dba
hpapier <hpapier@student.42.fr>
2018-06-04 23:39:19 +0200

    Wip2 db structure

--- api/database/config.js
+++ api/database/config.js
@@ -10,10 +10,34 @@
 // --> genre
 // --> sexual_orientation
 // --> bio
-// --> interests
+// --> interests_id
 // --> image_id
 // --> popularity_score
 // --> location
+// --> notification_id
 // --> is_complete
 
-// Table 
+// Table `images`
+// images:
+// --> id
+// --> user_id
+// --> path
+
+// Table `interests`
+// interest:
+// --> id
+// --> name
+
+// Table `user_interests`
+// user_interests:
+// --> id
+// --> user_id
+// --> interests_id
+
+// Table `notification`
+// notification:
+// --> id
+// --> is_viewed
+// --> action
+// --> user_id
+// --> from
