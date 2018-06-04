8f6aca0fa4f6c57ebad8967cfe5cfe9eb3d944e5
hpapier <hpapier@student.42.fr>
2018-06-04 15:48:20 +0200

    WIP Db structure

---
+++ api/database/config.js
@@ -0,0 +1,19 @@
+// Table `user`
+// user:
+// --> id
+// --> adress_email
+// --> username
+// --> last_name
+// --> first_name
+// --> password
+// --> is_confirmed
+// --> genre
+// --> sexual_orientation
+// --> bio
+// --> interests
+// --> image_id
+// --> popularity_score
+// --> location
+// --> is_complete
+
+// Table 
