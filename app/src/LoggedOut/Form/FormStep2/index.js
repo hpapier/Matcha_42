3d8a1ba3de65f9f521d1349df37dc4d883291442
hpapier <hpapier@student.42.fr>
2018-06-08 11:24:01 +0200

    LoggedOut part: registration -> WIP on visu

--- app/src/LoggedOut/Form/FormStep2/index.js
+++ app/src/LoggedOut/Form/FormStep2/index.js
@@ -17,10 +17,19 @@ class FormStep2 extends React.Component {
         this.day = this.day.bind(this);
         this.month = this.month.bind(this);
         this.year = this.year.bind(this);
+        this.genre = this.genre.bind(this);
+        this.search = this.search.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     day() {
-
+      let i = 1;
+      let array = []; 
+      while (i <= 31) {
+        array.push(i);
+        i++;
+      }
+      return array.map(item => <option value={item} key={item}>{item}</option>);
     }
 
     month() {
@@ -29,13 +38,34 @@ class FormStep2 extends React.Component {
     }
 
     year() {
+      let i = 2018;
+      let array = [];
+      while(i >= 1918) {
+        array.push(i);
+        i--;
+      }
+      return array.map(i => <option value={i} key={i}>{i}</option>);
+    }
 
+    genre() {
+      let array = ['Tu es..', 'Homme', 'Femme'];
+      return array.map(i => <option value={i} key={i}>{i}</option>);
+    }
+
+    search() {
+      let array = ['Selectionner', 'Homme', 'Femme', 'Autre'];
+      return array.map(i => <option value={i} key={i}>{i}</option>);
+    }
+
+    handleSubmit(e) {
+      e.preventDefault();
+      this.props.changeState.next();
     }
     
     render() {
       console.log(this.state);
         return (
-            <div id="form-step-2">
+            <div id="form-step-2" onSubmit={this.handleSubmit}>
                 <form id="form-step-2-form">
                     <div id="form-step-2-form-group-1">
                       <div id="form-step-2-form-group-1-box-1">
@@ -60,16 +90,22 @@ class FormStep2 extends React.Component {
                         </select>
                     </div>
                     <div id="form-step-2-form-group-3">
+                      <div id="form-step-2-form-group-3-box-1">
+                        <p id="form-step-2-form-group-3-genre-title">Sexe</p>
                         <select id="form-step-2-form-group-3-genre" type="text" onChange={e => this.setState({ genre: e.target.value })}>
                           {this.genre()}
                         </select>
+                      </div>
+                      <div id="form-step-2-form-group-3-box-2">
+                        <p id="form-step-2-form-group-3-lookingFor-title">Tu es ici pour...</p>
                         <select id="form-step-2-form-group-3-lookingFor" type="text" onChange={e => this.setState({ lookingFor: e.target.value })}>
                           {this.search()}
                         </select>
+                      </div>
                     </div>
                     <div id="form-step-2-form-group-4">
-                      <button id="form-step-2-form-submit" type="submit" onClick={this.props.changeState.next}>Next</button>
-                      <button id="form-step-2-previous" onClick={this.props.changeState.previous}>Previous</button>
+                      <button id="form-step-2-previous" type="button" onClick={this.props.changeState.previous}>Previous</button>
+                      <button id="form-step-2-form-submit" type="submit">Next</button>
                     </div>
                 </form>
             </div>
--- app/src/LoggedOut/Form/FormStep3/index.js
+++ app/src/LoggedOut/Form/FormStep3/index.js
@@ -14,11 +14,19 @@ class FormStep3 extends React.Component {
         return (
             <div id="form-step-3">
                 <form id="form-step-3-form">
+                  <div id="form-step-3-form-box-1">
+                    <label id="form-step-3-form-pwd-title">Mot de passe</label>
                     <input id="form-step-3-form-pwd" type="password" onChange={e => this.setState({ password: e.target.value })}/>
+                  </div>
+                  <div id="form-step-3-form-box-2">
+                    <label id="form-step-3-form-verif-pwd-title">Vérification du mot de passe</label>
                     <input id="form-step-3-form-verif-pwd" type="password" onChange={e => this.setState({ verif: e.target.value })}/>
-                    <button type="submit" id="form-step-3-form-submit">Valider</button>
+                  </div>
+                  <div id="form-step-3-form-box-3">
+                    <button id="form-step-3-previous" type="button" onClick={this.props.changeState}>Previous</button>
+                    <button id="form-step-3-submit" type="submit">Valider</button>
+                  </div>
                 </form>
-                <button id="form-step-3-previous" onClick={this.props.changeState}>Previous</button>
             </div>
         );
     }
