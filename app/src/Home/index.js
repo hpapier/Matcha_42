72b98392f7ad6fe8a25733c32490ef8a2ee3e2ba
hpapier <hpapier@student.42.fr>
2018-07-19 14:20:47 +0200

    Simple view for uncomplete profil

--- app/src/Home/index.js
+++ app/src/Home/index.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
 import DashboardPref from '../DashboardPref';
 import SelectionList from '../SelectionList';
+import UncompleteProfil from '../UncompleteProfil';
 import { connect } from 'react-redux';
 
 class Home extends Component {
   state = {};
 
+  UIAction = () => {
+    const { popularityScore } = this.props.userPref;
+    if (popularityScore < 10)
+      return false;
+    return true;
+  }
+
   render() {
     return (
       <div>
-        <DashboardPref />
-        <SelectionList />
+        {
+          (this.UIAction())
+          ?
+          (
+            <div>
+              <DashboardPref />
+              <SelectionList />
+            </div>
+          )
+          :
+          (
+            <div>
+              <UncompleteProfil />
+            </div>
+          )
+        }
       </div>
     );
   }
---
+++ app/src/UncompleteProfil/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './index.scss';
+
+const UncompleteProfil = () => (
+      <div id="uncomplete-profil">
+        <p id="uncomplete-profil-txt">Veuillez completer votre profil avant de pouvoir rencontrer de nouvelles personnes.</p>
+        <Link id="uncomplete-profil-btn" to="/preferences">Completer</Link>
+      </div>
+);
+
+export default UncompleteProfil;
