b4cee1bf9b38184233366ba483b39b8f1ef6de0d
hpapier <hpapier@student.42.fr>
2018-08-14 22:15:45 +0200

    LGO: SignUp Component: Structure and basic design

---
+++ app/src/LoggedOut/Homepage/AuthenticationBox/PasswordForgot/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.sass';
+
+class PasswordForgot extends Component {
+  render() {
+    return (
+      <div>
+        PasswordForgot Component
+      </div>
+    );
+  }
+};
+
+export default PasswordForgot;
--- app/src/LoggedOut/Homepage/AuthenticationBox/SignUp/index.js
+++ app/src/LoggedOut/Homepage/AuthenticationBox/SignUp/index.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import PasswordForgot from '../PasswordForgot';
+import personIcon from '../../../../../assets/person.svg';
+import lockIcon from '../../../../../assets/lock.svg';
 import './index.sass';
 
 class SignUp extends Component {
+  state = {
+    activeResetPwd: false
+  };
+
   render() {
+    const { activeResetPwd } = this.state;
     return (
-      <div>
-        SignUp Component
+      <div id='lgo-sign-up'>
+        <div id='lgo-sign-up-username'>
+          <img src={personIcon} alt='person-icon' id='lgo-sign-up-username-icon' />
+          <input type='text' placeholder="Nom d'utilisateur" id='lgo-sign-up-username-input' />
+        </div>
+
+        <div id='lgo-sign-up-password'>
+          <img src={lockIcon} alt='lock-icon' id='lgo-sign-up-password-icon' />
+          <input type='text' placeholder="Mot de passe" id='lgo-sign-up-password-input' />
+        </div>
+
+        <button id='lgo-sign-up-submit'>connexion</button>
+        <div onClick={() => this.setState({ activeResetPwd: !this.state.activeResetPwd })} id='lgo-sign-up-forgot'>Mot de passe oublié ?</div>
+        {
+          activeResetPwd ?
+          <PasswordForgot /> :
+          null
+        }
       </div>
     );
   }
