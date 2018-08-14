93eec13c966f1afc75e57c855e07b3fe36146b4b
hpapier <hpapier@student.42.fr>
2018-08-14 17:52:22 +0200

    Client: LoggedOut: SignIn component -> Structure and basic design

---
+++ app/src/LoggedOut/Homepage/AuthenticationBox/SignUp/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.sass';
+
+class SignUp extends Component {
+  render() {
+    return (
+      <div>
+        SignUp Component
+      </div>
+    );
+  }
+};
+
+export default SignUp;
--- app/src/LoggedOut/Homepage/AuthenticationBox/index.js
+++ app/src/LoggedOut/Homepage/AuthenticationBox/index.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import './index.sass';
 
-const AuthenticationBox = props => (
-  <div>
-    AuthenticationBox Component
-  </div>
-);
+class AuthenticationBox extends Component {
+  state = {
+    status: 'sign-in'
+  }
+
+  getActiveClassName = el => {
+    if (el === this.state.status)
+      return 'lgo-authentication-button-active';
+    else
+      return '';
+  }
+
+  render() {
+    return (
+      <div id='lgo-authentication-box'>
+        <div id='lgo-authentication-box-button'>
+          <div
+            onClick={() => (this.state.status !== 'sign-in') ? this.setState({ status: 'sign-in' }) : null}
+            className={`lgo-authentication-box-button left ${this.getActiveClassName('sign-in')}`}
+          >
+            inscription
+          </div>
+          <div
+            onClick={() => (this.state.status !== 'sign-up') ? this.setState({ status: 'sign-up' }) : null}
+            className={`lgo-authentication-box-button right ${this.getActiveClassName('sign-up')}`}
+          >
+            connexion
+          </div>
+        </div>
+        {
+          this.state.status === 'sign-in' ?
+          <SignIn /> :
+          <SignUp />
+        }
+      </div>
+    );
+  }
+};
 
 export default AuthenticationBox;
