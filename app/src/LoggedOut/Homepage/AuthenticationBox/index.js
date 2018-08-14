5866ed053198b4908f55c5f754a10d427187a183
hpapier <hpapier@student.42.fr>
2018-08-14 11:02:06 +0200

    Client: LoggedOut: Homepage component -> Structure and basic design

---
+++ app/src/LoggedOut/Homepage/AuthenticationBox/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AuthenticationBox = props => (
+  <div>
+    AuthenticationBox Component
+  </div>
+);
+
+export default AuthenticationBox;
--- app/src/LoggedOut/Homepage/index.js
+++ app/src/LoggedOut/Homepage/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import backgroundImage1 from '../../../assets/bg-1.jpg';
+import heartIcon from '../../../assets/heart.svg';
+import AuthenticationBox from './AuthenticationBox';
+import './index.scss';
 
 const Homepage = props => (
-  <div>
-    Homepage Component
+  <div id='lgo-homepage'>
+    <img id='lgo-homepage-background' src={backgroundImage1} alt='background' />
+    <div id='lgo-homepage-title'>
+      <div id='lgo-homepage-title-first'>Matcha</div>
+      <div id='lgo-homepage-title-second'>Rencontrez la personne qui partagera votre vie</div>
+    </div>
+    <AuthenticationBox />
+    <div id='lgo-homepage-box'>
+      <div id='lgo-homepage-box-title'>
+        <img id='lgo-homepage-box-title-icon' src={heartIcon} alt='love' />
+        <div id='lgo-homepage-box-title-text'>Le meilleur du matching en ligne</div>
+      </div>
+      <div id='lgo-homepage-box-subtitle'>De nombreux profils vérifié vous attendent sur Matcha.</div>
+      <div id='lgo-homage-box-copyright'>©hpapier 2017-2018</div>
+    </div>
   </div>
 );
 
