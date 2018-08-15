6ac527737fa92d9137f1d2b8d47b9ab699888765
hpapier <hpapier@student.42.fr>
2018-08-15 09:32:40 +0200

    LGO: PasswordForgot Component: Structure and basic design

--- app/src/LoggedOut/Homepage/AuthenticationBox/PasswordForgot/index.js
+++ app/src/LoggedOut/Homepage/AuthenticationBox/PasswordForgot/index.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import personIcon from '../../../../../assets/person.svg';
+import emailIcon from '../../../../../assets/email.svg';
 import './index.sass';
 
 class PasswordForgot extends Component {
   render() {
     return (
-      <div>
-        PasswordForgot Component
+      <div id='lgo-password-forgot'>
+        <div id='lgo-password-forgot-line'></div>
+
+        <div id='lgo-password-forgot-username'>
+          <img src={personIcon} alt='' id='lgo-password-forgot-username-icon' />
+          <input type='text' id='lgo-password-forgot-username-input' placeholder="Nom d'utilisateur" />
+        </div>
+
+        <div id='lgo-password-forgot-email'>
+          <img src={emailIcon} alt='' id='lgo-password-forgot-email-icon' />
+          <input type='text' id='lgo-password-forgot-email-input' placeholder='Adresse email' />
+        </div>
+
+        <button id='lgo-password-forgot-submit'>envoyer un email</button>
       </div>
     );
   }
